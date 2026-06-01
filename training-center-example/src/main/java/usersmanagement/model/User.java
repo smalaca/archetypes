9daa975b1752ce738b7c012eea62630bebdf247c
smalaca <sebastian.malaca@gmail.com>
2026-06-01 14:58:04 +0200

    add Person with its identifiers

--- training-center-example/src/main/java/usersmanagement/model/User.java
+++ training-center-example/src/main/java/usersmanagement/model/User.java
@@ -4,6 +4,11 @@ import com.smalaca.archetypes.annotations.ArchetypeParty;
 
 @ArchetypeParty.Person
 public class User {
-    private UserId userId;
-    private Login login;
+    private final UserId userId;
+    private final Login login;
+
+    public User(UserId userId, Login login) {
+        this.userId = userId;
+        this.login = login;
+    }
 }
