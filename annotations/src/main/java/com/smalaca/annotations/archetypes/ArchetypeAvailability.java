adc2e8c76d3bdf742fc0fa56d1ef2382d1135e4f
smalaca <sebastian.malaca@gmail.com>
2026-06-11 15:19:14 +0200

    add ArchetypeAvailability

---
+++ annotations/src/main/java/com/smalaca/annotations/archetypes/ArchetypeAvailability.java
@@ -0,0 +1,20 @@
+package com.smalaca.annotations.archetypes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ArchetypeAvailability {
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
+    @interface ReservationRequest {
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
+    @interface Reservation {
+    }
+}
